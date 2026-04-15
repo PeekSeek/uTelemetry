12a98d0d05a1326901c6915083f25e3cceeeb9ec
Ferret <kieran.van.der.heijde@gmail.com>
2026-04-15 16:21:16 +0200

    head

---
+++ README.md
@@ -0,0 +1,2 @@
+# uTelemetry
+Plugin developed by Homeless to provide more data for PvP than just kills.
---
+++ uTelemetry/PlayerStatsConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using Rocket.API;
+
+namespace uTelemetry
+{
+    public class Configuration : IRocketPluginConfiguration
+    {
+        public void LoadDefaults()
+        {
+        }
+    }
+}
